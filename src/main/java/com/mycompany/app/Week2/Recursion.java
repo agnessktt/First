de026d8d2d124d41c96aa9b8e54619ad72b670bf
Kiều Trang <160980419+agnessktt@users.noreply.github.com>
2024-02-29 11:05:52 +0000

    Recursion

---
+++ src/main/java/com/mycompany/app/Week2/Recursion.java
@@ -0,0 +1,13 @@
+package com.mycompany.app.Week2;
+
+public class Recursion {
+    // recursion method
+    public int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+}
+
