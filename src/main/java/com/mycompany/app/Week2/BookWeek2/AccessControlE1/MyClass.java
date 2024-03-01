0e16d44d12d217c0e01bc48d29bf3874fb887c8e
minhsangcoder <minhsangcoder@gmail.com>
2024-03-01 09:00:08 +0700

    Week 2 Exercise 1 In Book Access Control

---
+++ src/main/java/com/mycompany/app/Week2/BookWeek2/AccessControlE1/MyClass.java
@@ -0,0 +1,5 @@
+package com.mycompany.app.Week2.BookWeek2.AccessControlE1;
+
+public class MyClass {
+    //Class definition
+}
---
+++ src/main/java/com/mycompany/app/Week2/BookWeek2/AccessControlExercise1.java
@@ -0,0 +1,10 @@
+package com.mycompany.app.Week2.BookWeek2;
+
+import com.mycompany.app.Week2.BookWeek2.AccessControlE1.MyClass;
+
+public class AccessControlExercise1 {
+    public static void accesscontrolexercise1 () {
+        MyClass myObject = new MyClass();
+        
+    }
+}
