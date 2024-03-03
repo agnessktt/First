981b29ef593a2732ea4f310ac5eacf908e742559
Kiều Trang <160980419+agnessktt@users.noreply.github.com>
2024-03-03 16:34:52 +0000

    interfaceExercise8

---
+++ src/main/java/com/mycompany/app/Week2/interfaceExercise8.java
@@ -0,0 +1,30 @@
+package com.mycompany.app.Week2;
+
+public interface FastFood {
+    double getPrice();
+    int getCalories();
+    String getDescription();
+}
+
+public class Sandwich implements FastFood {
+    private String name;
+    private double price;
+    private int calories;
+
+    //existing constructor and other methods
+
+
+    public double getPrice() {
+        return price;
+    }
+
+  
+    public int getCalories() {
+        return calories;
+    }
+
+   
+    public String getDescription() {
+        return name + " sandwich";
+    }
+}
