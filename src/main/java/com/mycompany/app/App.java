583fafcb873eb60d3e7f1567f535a7cea05fe9c5
diulih <160729383+diulh@users.noreply.github.com>
2024-03-08 02:02:05 +0000

    Week3Code1

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -33,5 +33,6 @@ public class App {
         System.out.println(myMethod4.myMethodFour(3));
         myMethod5.checkAge(20);
         debug.debug1 ("Ukamo");
+        
     }
 }
---
+++ src/main/java/com/mycompany/app/Week3/TestArithmetic.java
@@ -0,0 +1,5 @@
+package com.mycompany.app.Week3;
+
+public class TestArithmetic {
+    
+}
